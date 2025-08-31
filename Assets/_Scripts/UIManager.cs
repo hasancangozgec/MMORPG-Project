1d7820d7d916b5d3cbd4e2bb17ac36a4744971fe
Hasancan GÖZGEÇ <84378147+hasancangozgec@users.noreply.github.com>
2025-08-31 12:02:35 +0300

    First-Update
    İlk güncellemeler ile başladı.

---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject loginPanel;
+    public GameObject registerPanel;
+
+    void Start()
+    {
+        ShowLoginPanel();
+    }
+
+    public void ShowLoginPanel()
+    {
+        loginPanel.SetActive(true);
+        registerPanel.SetActive(false);
+    }
+
+    public void ShowRegisterPanel()
+    {
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(true);
+    }
+}
